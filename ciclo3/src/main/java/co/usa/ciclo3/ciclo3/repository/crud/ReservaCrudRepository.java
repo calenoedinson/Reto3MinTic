53fda5db599932434050e2636bc9c5201b16775f
Edinson Caleno <edinson.caleno.mt@correo.usa.edu.co>
2021-10-29 05:25:05 -0500

    bien

--- ciclo3/src/main/java/co/usa/ciclo3/ciclo3/repository/crud/ReservaCrudRepository.java
+++ ciclo3/src/main/java/co/usa/ciclo3/ciclo3/repository/crud/ReservaCrudRepository.java
@@ -12,6 +12,6 @@ public interface ReservaCrudRepository extends CrudRepository<Reserva, Integer> 
     public List<Reserva> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
     
     // select clientId, count(*) as "total" from reservacion group by cliendId order by total desc;
-    @Query ("SELECT c.client, COUNT(c.client) from Reservacion AS c group by c.client order by COUNT(c.client)DESC")
+    @Query ("SELECT c.client, COUNT(c.client) from Reserva AS c group by c.client order by COUNT(c.client)DESC")
     public List<Object[]> countTotalReservationsByClient();
 }
